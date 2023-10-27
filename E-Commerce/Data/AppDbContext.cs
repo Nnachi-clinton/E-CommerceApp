a59c4db422dd65e922595eb8a7f17e086a908cb2
Nnachi-clinton <clinton.amadi@decagon.dev>
2023-10-27 03:42:18 +0100

    Add project files.

---
+++ E-Commerce/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
