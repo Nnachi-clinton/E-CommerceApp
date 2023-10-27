e11120d86679450ed83ed2456906566b8fafec8d
Nnachi-clinton <clinton.amadi@decagon.dev>
2023-10-27 10:57:43 +0100

    Crud operations completed

---
+++ E-Commerce/Controllers/CategoryController.cs
@@ -0,0 +1,69 @@
+﻿using E_Commerce.Data;
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var getAll = _db.Categories.ToList();
+            return View(getAll);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "name and displayorder cannot match.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+           
+        } 
+        public IActionResult Edit(int Id)
+        {
+            if (Id==null || Id ==0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.FirstOrDefault(c => c.Id == Id);
+            if (category == null)
+            {
+                return BadRequest();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+           
+        }
+    }
+}
